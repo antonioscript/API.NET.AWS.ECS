ee84696d3cbb829321f370ee039d569bae053d43
Antônio Rocha <antoniojunior159@gmail.com>
2025-05-28 23:47:50 -0300

    Create Project

---
+++ src/BookManagerSolution/BookManager/Data/MongoDBConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace BookManager.Data;
+public class MongoDBConfig
+{
+    public string? BookCollectionName { get; set; }
+    public string? ConnectionString { get; set; }
+    public string? DatabaseName { get; set; }
+}
--- src/BookManagerSolution/BookManager/Program.cs
+++ src/BookManagerSolution/BookManager/Program.cs
@@ -1,13 +1,24 @@
+using BookManager.Data;
+using BookManager.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Mongo
+builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection("MongoDBConfig"));
+builder.Services.AddScoped<BookService>();
+
+builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
